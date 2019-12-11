44c3ddb555e940bd93e6cba1982d63cb2ab76022
Hugo Sales <someonewithpc@gmail.com>
2019-12-11 12:16:02 +0000

    Refactoring

---
+++ compiler.hs
@@ -0,0 +1,17 @@
+module Main where
+import Lexer
+import Parser
+
+
+compile t = undefined
+
+main :: IO ()
+main = do
+        raw_input <- getContents
+        let token_list = scan_tokens raw_input
+        let parse_tree = parse token_list
+
+        putStrLn ("Token List:\n" ++ (show token_list) ++ "\n")
+        putStrLn ("Parse Tree:\n" ++ (printTree parse_tree))
+
+        compile parse_tree
